01a2b5960cc47426105295715c78dde852aada3c
Joseph Schefer <joseph.schefer@gmail.com>
2017-12-07 17:23:53 -0500

    db config

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
 var mysql = require("mysql");
 
+// mysql://b0e64bf740933a:76818c4b@us-cdbr-iron-east-05.cleardb.net/heroku_bfcd5d8ae4c847d?reconnect=true
+
 var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "g$HBB9VV",
-  database: "burgers_db"
+  host: "us-cdbr-iron-east-05.cleardb.net",
+  user: "b0e64bf740933a",
+  password: "76818c4b",
+  database: "heroku_bfcd5d8ae4c847d"
 });
 
 connection.connect(function(err) {
